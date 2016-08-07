5eaecd447e3bce88a01bf10b3592d8088dd9e545
Hutsh <hxbbun@gmail.com>
2016-08-07 20:33:29 +0800

    Fixed file
    Fixed file

--- js/toeflparctice.js
+++ js/toeflparctice.js
@@ -72,16 +72,44 @@ function end() {
 
 }
 
+var secondcountdown=15;
+var flag45s=0;
+function showtime() {
+    if(secondcountdown>44) {
+        flag45s = 1;
+    }
+    secondcountdown--;
+    document.getElementById("time-count").style.display="inline-block";
+    document.getElementById("time-count").innerHTML = secondcountdown + "s";
+    if(secondcountdown==0){
+        if(flag45s){
+            document.getElementById("time-count").innerHTML = "This is the and of the practice, you can download your answer by clicking the button bellow. Thanks for using!";
+        }
+        else{
+            document.getElementById("time-count").innerHTML = "Please begin speaking after the beep.";
+        }
+
+        return;
+    }
+    setTimeout("showtime()",1000);
+}
 function count15() {
     var bar = document.getElementById("timer15");
+    var t = document.getElementById("time-count");
     bar.style.display="block";
+    t.style.display="block";
     setTimeout(function(){
         var progressbar15 = document.getElementById("timer15-bar");
         progressbar15.style.width="100%";
     },100);
 
+    secondcountdown=16;
+    showtime();
+
     setTimeout("count45();",15000);//!!!!!!!!!!!应该15000
 }
+
+
 
 function count45() {
     var spa = document.getElementById('speakaudio');
@@ -110,6 +138,9 @@ function record45() {
     console.log("recoding");
     var button = document.getElementById("record");
     toggleRecording(button);
+
+    secondcountdown=46;
+    showtime();
     setTimeout(function(){toggleRecording(button); showsave();},45000);//ying45000
 }
 
